c836d6eb75753a7836cfd8c6a274aa2ab8f9aa3a
tty3 <johndeer1969@pm.me>
2022-05-15 19:50:13 -0400

    Edited README.md and added install.sh

--- README.md
+++ README.md
@@ -1,23 +1,6 @@
-## Install Deps
+## Install
 ```
-# Update the system (probably not needed but idc)
-sudo pacman -Syu
-
-# Install some deps and other things
-sudo pacman -S imagemagick util-linux scrot i3lock terminator zsh 
-
-# Install something to make i3lock better
-git clone https://github.com/meskarune/i3lock-fancy.git
-cd i3lock-fancy
-sudo make install
-
-# Install something to make i3lock-fancy better
-git clone https://github.com/guimeira/i3lock-fancy-multimonitor.git
-cp -r i3lock-fancy-multimonitor $HOME/.i3
-chmod +x $HOME/.i3/i3lock-fancy-multimonitor/lock
-
-# echo "bindsym \$mod+p exec $HOME/.i3/i3lock-fancy-multimonitor/lock" >> $HOME/.i3/config
-
-# Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd dotfiles
+chmod +x install.sh
+./install.sh
 ```
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Update the system (probably not needed but idc)
+sudo pacman -Syu
+
+# Install some deps and other things
+sudo pacman -S imagemagick util-linux scrot i3lock rofi terminator zsh
+
+# Copy configs
+cd $HOME/dotfiles/.config/i3
+cp config $HOME/.config/i3/
+cd $HOME/dotfiles/config/terminator
+cp config $HOME/.config/terminator/
+cd $HOME/dotfiles/.config/rofi
+cp rofidmenu.rasi $HOME/.config/rofi/
+cp arc_dark_colors.rasi $HOME/.config/rofi/
+cp arc_dark_transparent_colors.rasi $HOME/.config/rofi/
+cp powermenu.rasi $HOME/.config/rofi/
+
+# Install something to make i3lock better
+cd $HOME
+git clone https://github.com/meskarune/i3lock-fancy.git
+cd i3lock-fancy
+sudo make install
+
+# Install something to make i3lock-fancy better
+cd $HOME
+git clone https://github.com/guimeira/i3lock-fancy-multimonitor.git
+cp -r i3lock-fancy-multimonitor $HOME/.i3
+chmod +x $HOME/.i3/i3lock-fancy-multimonitor/lock
+echo "bindsym \$mod+p exec $HOME/.i3/i3lock-fancy-multimonitor/lock" >> $HOME/.i3/config
+
+# Install ohmyzsh and theme
+cd $HOME
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd $HOME/dotfiles/.ohmyzsh/custom/themes/
+cp bash.zsh-theme $HOME/.ohmyzsh/custom/themes/
